58a36c879381ec466bbe85809bf0b7662d4a5e98
wontae <wjs152828@gmail.com>
2022-08-21 08:41:15 +0900

    Initial Commit

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ class BlogPost(db.Model):
     subtitle = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
-    img_url = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(300), nullable=False)
     comments = relationship("Comment", back_populates="parent_post")
 # db.create_all()
 
